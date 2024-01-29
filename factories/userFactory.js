5ed2d6329125d2d751563e9e1e40bd0b74a9e5dd
Oscar Moreno <oscarmorenoarenas72@gmail.com>
2024-01-29 20:36:06 +0100

    password seeder

--- factories/userFactory.js
+++ factories/userFactory.js
@@ -6,13 +6,13 @@ const userFactory = async (ctos = 2) => {
     let factory = []
     let names=['root','user']
     for(let i = 0; i < ctos; i++) {
-        const password = await bcrypt.hash('1234', 10);
+        const password = await bcrypt.hash('password', 10);
         let u = 
             {
             first_name: names[i],
             last_name: names[i],
             email: names[i]+'@'+names[i]+'.com',
-            password: 'password',
+            password: password,
             createdAt: new Date(),
             updatedAt: new Date()
         }
